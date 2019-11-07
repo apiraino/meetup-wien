c1bdf625d52dcd4bec53fd26e66c25a0457a39e3
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-11-07 17:30:25 +0100

    Add some more code.

---
+++ dynamic_vs_static.rs
@@ -0,0 +1,42 @@
+﻿// Type your code here, or load an example.
+use std::fmt::Debug;
+
+pub trait Weapon<T>
+where T: Debug {
+    fn hit(&self, target: T);
+}
+
+// equals to:
+// trait Weapon {
+//     fn hit(&self, target: impl Debug);
+// }
+
+pub struct Sword;
+
+impl<T> Weapon<T> for Sword
+where T: Debug {
+    fn hit(&self, target: T) {
+        println!("Chopped {:?} clean in half", target);
+    }
+}
+
+pub struct Samuray<'w, T> where T: Debug {
+    weapon: Box<dyn Weapon<T> + 'w>,
+}
+
+impl<'w, T> Samuray<'w, T>
+where T: Debug {
+    pub fn new(weapon: impl Weapon<T> + 'w) -> Self {
+        Samuray{ weapon: Box::new(weapon) }
+    }
+    
+    pub fn attack(&self, target: T) {
+        self.weapon.hit(target);
+    }
+}
+
+pub fn main() {
+    let samuray = Samuray::new(Sword{});
+    samuray.attack("Akuna Matata");
+    //samuray.attack(32);
+}
---
+++ zero_cost_zero_code.rs
@@ -0,0 +1,47 @@
+﻿// Type your code here, or load an example.
+pub trait Trans<S>
+where S: State,
+      Self: State,
+{
+    fn next(self) -> S;
+}
+
+pub trait End<S>
+where
+    S: State,
+    Self: State,
+{
+    fn end(self) -> S;
+}
+
+pub trait State{}
+
+pub struct Start;
+
+impl State for Start {}
+
+pub struct Next;
+
+impl State for Next {}
+
+impl Trans<Next> for Start {
+    fn next(self) -> Next {
+        Next {}
+    }
+}
+
+pub struct EndState;
+
+impl State for EndState {}
+
+impl End<EndState> for Next {
+    fn end(self) -> EndState {
+        EndState {}
+    }
+}
+
+pub fn do_it() {
+    let start = Start {};
+    let next: Next = start.next();
+    let _ : EndState = next.end();
+}
